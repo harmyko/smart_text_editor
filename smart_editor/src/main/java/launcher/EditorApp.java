e62fa4b19698fedbf150c2bf13688cdcb7f1f5e8
harmyko <ugniusteiserskis2005@gmail.com>
2025-05-20 19:26:07 +0300

    Lab #8: Generated javadoc.

--- smart_editor/src/main/java/launcher/EditorApp.java
+++ smart_editor/src/main/java/launcher/EditorApp.java
@@ -3,7 +3,19 @@ package main.java.launcher;
 import main.java.text_editor.gui.EditorGUI;
 import javax.swing.*;
 
+/**
+ * The entry point for the text editor application.
+ * <p>
+ * This class launches the GUI for the text editor using the {@link EditorGUI} class.
+ * @author Ugnius Teišerskis
+ */
 public class EditorApp {
+
+    /**
+     * The main method that starts the text editor application.
+     *
+     * @param args the command-line arguments (not used)
+     */
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new EditorGUI());
     }
